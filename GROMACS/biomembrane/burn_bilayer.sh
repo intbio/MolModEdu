2ab921bc21184191beb75b7720e9bd66c91ee9e4
GlebNovikov <gnovikov87@gmail.com>
2018-11-19 16:36:45 +0100

    Update burn_bilayer.sh

--- GROMACS/biomembrane/burn_bilayer.sh
+++ GROMACS/biomembrane/burn_bilayer.sh
@@ -7,7 +7,6 @@
 # here the methodological tutorial of the selection of barostat and thermostat: http://molsim.org/ru/methods/MD_basics_2
 # After execution the script takes gro, ndx and topol top files (from ./ref/) and creates input files required for GROMACS (mdp)
 # as well as input script (run.pbs) to run the simulations on the mpi server ( should be adapted for your server!!!).
-# NB! Check the strings 153 and 199 of the script to adapt it for your server and version of gromacs!
 #
 #!/bin/bash
 
@@ -33,74 +32,25 @@ cpus='32' # number of cpus, which will be reserved for each simulation on the se
 sim_time='1-10:30:00' # time limit of simulation: 1 day, 10 hours and 30 minutes
 
 # Here we define range of the temperature, which will be used for each simulation
-mega_array=('323' '350' '400' '450' '500'); # temprerature in K
+temp_array=('323' '350' '400' '450' '500'); # temprerature in K
 
 
 # we take an input gro file with the gromacs topology and ndx file and prepare for it MDP files as well as run.pbs file
 # this is begining of replication loop
 for ref_md in ${ref_folder}/popc_bilayer; do
   ref_title=$(basename "$ref_md")
-  echo "System ${ref_title} will be replicated "${#mega_array[*]}" times"
+  echo "System ${ref_title} will be replicated "${#temp_array[*]}" times"
   sleep 2
   server=${output_sys}/${project}/simulations
   output="${server}"
   mkdir  ${root}
   mkdir ${server}
   cd ${server}
-for i in "${mega_array[@]}"
+for i in "${temp_array[@]}"
 do
    cp -r ${ref_md} ${output}/${project}_${i}K
    cd ${output}/${project}_${i}K
-# print mpd files for the equilibration: step-by-step increase temperature to equilibrate system in NPT
-# since we use several steps of the equilibration we can script it in loop
-# adapt for your protocol and than change it in run.pbs
-  for step in {1..5}; do
-  let "temp=310 + (${step} * 10)"
-  printf "integrator              = md
-dt                      = 0.002
-nsteps                  = 500000
-nstlog                  = 5000
-nstxout                 = 50000
-nstxout-compressed      = 5000
-nstvout                 = 5000
-nstfout                 = 5000
-nstcalcenergy           = 100
-nstenergy               = 5000
-;
-cutoff-scheme           = Verlet
-nstlist                 = 20
-rlist                   = 1.2
-coulombtype             = pme
-rcoulomb                = 1.2
-vdwtype                 = Cut-off
-vdw-modifier            = Force-switch
-rvdw_switch             = 1.0
-rvdw                    = 1.2
-;
-tcoupl                  = Nose-Hoover
-tc_grps                 = MEMB   SOL_ION
-tau_t                   = 1.0    1.0
-ref_t                   = ${temp} ${temp}
-;
-pcoupl                  = Parrinello-Rahman
-pcoupltype              = semiisotropic
-tau_p                   = 5.0
-compressibility         = 4.5e-5  4.5e-5
-ref_p                   = 1.0     1.0
-gen_vel                 = no
-;
-constraints             = h-bonds
-constraint_algorithm    = LINCS
-continuation            = Yes
-;
-nstcomm                 = 100
-comm_mode               = linear
-comm_grps               = MEMB   SOL_ION
-;
-refcoord_scaling        = com" > "${output}/${project}_${i}K/equil${step}.mdp"
-done
-
-  # print mpd file for production run of 50 ns;
+  # print mpd file for production run;
   printf "integrator              = md
 dt                      = 0.002
 nsteps                  = 25000000 ; 50ns test production run
@@ -146,29 +96,15 @@ refcoord_scaling        = com
 ; Deformation is off
 ; Deform = 0 0 0 0 0 0" > "${output}/${project}_${i}K/production_${i}K.mdp"
 
-
    # print script file to submit job on Q server
    # !!should be adapted for specific server
    printf "#PBS -N ${ref_title}_${i}.dynamics
-#PBS -l nodes=1:ppn=${cpus}           #один узел, 32 процессора
+#PBS -l nodes=1:ppn=${cpus}           #1 node, 32 cpus
 #PBS -q day
 #PBS -V
 
-
-# equilibrate system step-by-step increasing temperature
-gmx grompp -f equil1.mdp -o equil_${i}K_1.tpr -c System.gro -t System.cpt -n System.ndx -p System.top -maxwarn -1
-mpirun -np \${NB_TASKS} mdrun_mpi -v -deffnm equil_${i}K_1
-gmx grompp -f equil2.mdp -o equil_${i}K_2.tpr -c equil_${i}K_1 -t equil_${i}K_1 -n System.ndx -p System.top -maxwarn -1
-mpirun -np \${NB_TASKS} mdrun_mpi -v -deffnm equil_${i}K_2
-gmx grompp -f equil3.mdp -o equil_${i}K_3.tpr -c equil_${i}K_2 -t equil_${i}K_2 -n System.ndx -p System.top -maxwarn -1
-mpirun -np \${NB_TASKS} mdrun_mpi -v -deffnm equil_${i}K_3
-gmx grompp -f equil4.mdp -o equil_${i}K_4.tpr -c equil_${i}K_3 -t equil_${i}K_3 -n System.ndx -p System.top -maxwarn -1
-mpirun -np \${NB_TASKS} mdrun_mpi -v -deffnm equil_${i}K_4
-gmx grompp -f equil5.mdp -o equil_${i}K_5.tpr -c equil_${i}K_4 -t equil_${i}K_4 -n System.ndx -p System.top -maxwarn -1
-mpirun -np \${NB_TASKS} mdrun_mpi -v -deffnm equil_${i}K_5
-
-# run production run for 50 ns
-gmx grompp -f production_${i}K.mdp -o ${project}_${i}K_0.tpr -c equil_${i}K_5 -t equil_${i}K_5 -n System.ndx -p System.top -maxwarn -1
+# run production run for 50 ns at ${i}K
+gmx grompp -f production_${i}K.mdp -o ${project}_${i}K_0.tpr -c System.gro -t System.cpt -n System.ndx -p System.top -maxwarn -1
 mpirun -np \${NB_TASKS} mdrun_mpi -v -deffnm ${project}_${i}K_0" > "${output}/${project}_${i}K/run.pbs"
 
 done
@@ -178,7 +114,7 @@ done
 
 # print submit.sh script to run all jobs in parallel on multi-CPU server
 
-# this script submit all md jobs in parallel
+# this script will submit all md jobs in parallel
 {
   ## print the header, and substitute our own value for HOME
   printf '#!/bin/bash\n'
